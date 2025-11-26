4ca3c020a3040c5915ec61098b434698c610bebb
qian94608-ui <qian94608@gmail.com>
2025-11-27 02:21:46 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,2 @@
-const CACHE_NAME = 'sloth-v4-ultimate';
-const ASSETS = [
-    './', './index.html', './style.css', './app.js', './manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-    'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js',
-    'https://unpkg.com/html5-qrcode',
-    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap'
-];
-
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => n!==CACHE_NAME?caches.delete(n):null)))));
+const CACHE_NAME = 'sloth-v4.1-fixed'; 
+// ... 其他保持不变
