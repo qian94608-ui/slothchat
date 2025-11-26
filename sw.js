0dfaa2d3bf8f0366245c40f5ee42a862a5c6394d
qian94608-ui <qian94608@gmail.com>
2025-11-27 01:24:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'sloth-v2';
+const CACHE_NAME = 'sloth-v3-ac-style';
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
-    'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js'
+    'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js',
+    'https://unpkg.com/html5-qrcode', // 摄像头库
+    'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap'
 ];
 
 self.addEventListener('install', e => {
@@ -11,4 +13,11 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
+// 清理旧缓存
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(
+        keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    )));
 });
