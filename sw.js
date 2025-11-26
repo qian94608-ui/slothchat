6a2db5e8f97a261ed08d09f4fd539eb3dfec5ff4
qian94608-ui <qian94608@gmail.com>
2025-11-27 01:43:41 +0800

    Update service worker cache name and files

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'sloth-v3-ac-style';
+const CACHE_NAME = 'sloth-kakao-v1'; // 升级版本号
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
     'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js',
-    'https://unpkg.com/html5-qrcode', // 摄像头库
-    'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap'
+    'https://unpkg.com/html5-qrcode',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
@@ -15,7 +15,6 @@ self.addEventListener('fetch', e => {
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
-// 清理旧缓存
 self.addEventListener('activate', e => {
     e.waitUntil(caches.keys().then(keys => Promise.all(
         keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
