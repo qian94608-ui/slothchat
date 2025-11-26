9013cddb517c5387d38afab8ceaa2ae324323258
qian94608-ui <qian94608@gmail.com>
2025-11-27 01:54:44 +0800

    Update service worker cache and asset list

--- sw.js
+++ sw.js
@@ -1,22 +1,12 @@
-const CACHE_NAME = 'sloth-kakao-v1'; // 升级版本号
-const FILES = [
+const CACHE_NAME = 'sloth-v4-ultimate';
+const ASSETS = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
     'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js',
     'https://unpkg.com/html5-qrcode',
-    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap'
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap'
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
-
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(
-        keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
-    )));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => n!==CACHE_NAME?caches.delete(n):null)))));
