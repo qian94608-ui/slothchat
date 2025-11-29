537cfe08791c492f728efd0c834838ecbdc0baeb
qian94608-ui <qian94608@gmail.com>
2025-11-29 21:00:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'wojak-v15-hybrid-core';
+const CACHE_NAME = 'const CACHE_NAME = 'wojak-cs-v1';
+// ... 文件列表同上 ...';
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
@@ -8,3 +9,4 @@ const FILES = [
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => n!==CACHE_NAME?caches.delete(n):null)))));
+
