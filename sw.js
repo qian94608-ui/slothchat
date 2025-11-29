5241b20b010a574f449f83bd49c1da3c050bb1ec
qian94608-ui <qian94608@gmail.com>
2025-11-29 17:26:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'wojak-v8-voice-gesture';
+const CACHE_NAME = 'wojak-v9-instant';
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
     'https://unpkg.com/peerjs@1.5.1/dist/peerjs.min.js',
     'https://unpkg.com/html5-qrcode'
 ];
-
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => n!==CACHE_NAME?caches.delete(n):null)))));
