1a38dde97a154dafed8e4be2d31522e592c238da
qian94608-ui <qian94608@gmail.com>
2025-11-29 17:18:15 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wojaknet-v7-persistent';
+const CACHE_NAME = 'wojak-v8-voice-gesture';
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
