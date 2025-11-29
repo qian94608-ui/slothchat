d0e462b6e7810c3a4e50cc5770e3046563773e57
qian94608-ui <qian94608@gmail.com>
2025-11-29 22:02:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wojak-v17-fixed';
+const CACHE_NAME = 'wojak-v18-debugged';
 const FILES = [
     './', './index.html', './style.css', './app.js', './manifest.json',
     'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
